556d755c7d6138229967f7f1e0130ed17a8f84a8
Aryxon04 <76776871+AryxonDazmor@users.noreply.github.com>
2026-01-06 12:53:42 +0000

    Added card, fixed UI

--- src/components/Section/fiturtest.tsx
+++ src/components/Section/fiturtest.tsx
@@ -11,7 +11,7 @@ export const Embed = ({header,embed,footer}: forembed) => {
     return(
         <div style={{
             padding: '8px',
-            backgroundColor: 'cyan',
+            backgroundColor: 'white',
             display: 'flex',
             flexDirection: 'column', 
             justifyContent: 'center', 
@@ -21,7 +21,7 @@ export const Embed = ({header,embed,footer}: forembed) => {
             marginRight: '20%',
             borderRadius: '16px'
         }}>
-            <h1 style = {{color: 'black', margin: '8px',backgroundColor:'green'}}>{header}</h1>
+            <h1 style = {{color: 'black', margin: '8px'}}>{header}</h1>
            <iframe    
                 src = {embed}
                 width = '75%'
@@ -29,6 +29,34 @@ export const Embed = ({header,embed,footer}: forembed) => {
                 style = {{border: 'none'}}
             />
             <p style = {{color: 'black', margin: '8px'}}>{footer}</p>
+        </div>
+    )
+    
+}
+
+type forbox = {
+    image: string;
+    title: string;
+    subtitle: string;
+    button: string;
+    kode: string;
+}
+
+export const Box = ({image, title, subtitle, button, kode}: forbox) => {
+    return(
+        <div style={{
+            padding: '10px',
+            backgroundColor: '#d8d8d8ff',
+            display: 'flex',
+            flexDirection: 'column', 
+            borderRadius: '10px',
+            height: 'auto',
+            width: '100%',
+        }}>
+            <img className = "w-full h-48 object-cover object-top"src = {image}/>
+            <h1 className = "text-black text-lg text-left mt-2">{kode} {title}</h1>
+            <h1 className = "text-gray-500 text-sm text-left">{subtitle}</h1>
+            <a href={button} className = "px-8 py-2 rounded-sm text-black bg-blue-600 w-full text-center text-bold mt-auto">Enter</a>
         </div>
     )
     
