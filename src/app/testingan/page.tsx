d3670fb4f222398be46e1282c0503626c44cb313
Aryxon04 <76776871+AryxonDazmor@users.noreply.github.com>
2026-01-08 14:43:55 +0000

    Updated Library

--- src/app/testingan/page.tsx
+++ src/app/testingan/page.tsx
@@ -5,21 +5,21 @@ export default function Page() {
     <div>
       <h1> Nah ini </h1>
       <div>
-        
-            <div style ={{width: '50%', height: '600px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '60px', margin:'0 auto'}}>
-                <h1>Anzio</h1>
-                <iframe 
-                    src = "https://drive.google.com/file/d/1DTpNSrUdDPURwLivlQn0UfuenvZm7Hp0/preview"
-                    width = '100%'
-                    height = '100%'
-                    style = {{border: 'none'}}
-                />
-            </div>
             <div>
             <Embed
               header = "Anchovy"
               embed = "https://drive.google.com/file/d/1DTpNSrUdDPURwLivlQn0UfuenvZm7Hp0/preview"
-              footer = "AA"
+              footer = "Duce of Anzio"
+            />
+            <Embed
+              header = 'Pepperoni'
+              embed = "https://drive.google.com/file/d/1kmeHUF500owRJO9OuJENKH6xg-CO5o2t/preview"
+              footer= 'Amba'
+            />
+            <Embed
+              header = 'Try Youtube'
+              embed = 'https://www.youtube.com/embed/PqJUTfxRS0M'
+              footer = 'idk'
             />
             </div>
       </div>
