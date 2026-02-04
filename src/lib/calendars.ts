d77eacffb1951ca90b1af1ac3fa6218588f06c02
nikechase3 <85381840+nikechase3@users.noreply.github.com>
2026-02-04 13:13:36 +0700

    Update calendars.ts

--- src/lib/calendars.ts
+++ src/lib/calendars.ts
@@ -6,21 +6,32 @@ export type CalendarConfig = {
 
 export const calendars: CalendarConfig[] = [
   {
-    id: 'classes',
-    name: 'Class Schedule',
+    id: 'TEKNIK KIMIA',
+    name: 'TEKNIK KIMIA',
     embedUrl:
-      'https://calendar.google.com/calendar/embed?src=c_5dbf12dbc1c0445c7a47b4a4e70c0e3ab17a49f11d3446a30b1f9b9fed7b8961%40group.calendar.google.com&ctz=UTC',
+      'https://calendar.google.com/calendar/embed?src=c_1098453c7b34ca53a9034687f53cee2ba7a89580e54ed068738cd0f3e44c4c85%40group.calendar.google.com&ctz=Asia%2FBangkok',
   },
   {
-    id: 'exams',
-    name: 'Exams',
+    id: 'TEKNIK BIOENERGI',
+    name: 'TEKNIK BIOENERGI',
     embedUrl:
-      'https://calendar.google.com/calendar/embed?src=c_5dbf12dbc1c0445c7a47b4a4e70c0e3ab17a49f11d3446a30b1f9b9fed7b8961%40group.calendar.google.com&ctz=UTC',
+      'https://calendar.google.com/calendar/embed?src=c_2d694a9b295230d7417609d794f40b5f132e904b640dbb875d15a8e66c5902c8%40group.calendar.google.com&ctz=Asia%2FBangkok',
   },
   {
-    id: 'events',
-    name: 'Academic Events',
+    id: 'TEKNIK PANGAN',
+    name: 'TEKNIK PANGAN',
     embedUrl:
-      'https://calendar.google.com/calendar/embed?src=c_5dbf12dbc1c0445c7a47b4a4e70c0e3ab17a49f11d3446a30b1f9b9fed7b8961%40group.calendar.google.com&ctz=UTC',
-  },
+      'https://calendar.google.com/calendar/embed?src=c_1ca113cfd77a0990eb9238495ff8883ff134b96a6e3e7078ad008eb96aa7b2ba%40group.calendar.google.com&ctz=Asia%2FBangkok',
+  },{id: 'TEKNIK INDUSTRI(G)',
+    name: 'TEKNIK INDUSTRI(G)',
+    embedUrl:
+      'https://calendar.google.com/calendar/embed?src=c_caf031acdcf1e985f0590ce5a4a0bebf817755bc30a27ccd43425e06b8200e27%40group.calendar.google.com&ctz=Asia%2FBangkok',},
+  {id: 'MANAJEMEN REKAYASA',
+    name: 'MANAJEMEN REKAYASA',
+    embedUrl:
+      'https://calendar.google.com/calendar/embed?src=c_7b2b4c3503dc067be9b8d7457a6f4506515fd2ce2041f4726d33642b271c8077%40group.calendar.google.com&ctz=Asia%2FBangkok',},
+  {id: 'TEKNIK INDUSTRI(C)',
+    name: 'TEKNIK INDUSTRI(C)',
+    embedUrl:
+      'https://calendar.google.com/calendar/embed?src=c_e1221f1888d50b6b6bf8c1906ef35fd1228ece31b29ded1e7a8da0ee34acc062%40group.calendar.google.com&ctz=Asia%2FBangkok',}
 ];
